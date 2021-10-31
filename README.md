371eea96c19ae368266f78e521971e9231e53680
augustine <augustino0890@gmail.com>
2021-10-31 14:48:43 +0900

    devices: devices.json database and list devices endpoint

--- README.md
+++ README.md
@@ -3,5 +3,16 @@
 ## Project Structure
 ## Introduction
 - The REST API to get the devices
-
+- The list of HTTP endpoints
+ - GET `/devices`: returns a list of devices
+ - POST `/devices`: creates a new devices
+ - GET `/devices/{id}`: returns an existing device by ID
+ - GET `/devices/{type}: returns a list of devices by type
+ - GET `/devices/{status}: returns a list of devices by status
+ 
 ## Run the Server
+
+## Dependencies
+- Install the dependencies used by the project
+- Gin Web Framework
+  - `go get -u github.com/gin-gonic/gin`
--- main.go
+++ main.go
@@ -1,6 +1,51 @@
-package devices
+package main
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Define the data model
+type Device struct {
+	ID          string    `json:"id"`
+	Type        string    `json:"type"`
+	Coordinates []float32 `json:"coordinates"`
+	Status      string    `json:"status"`
+	Timezone    string    `json:"timezone"`
+}
+
+type Results map[string]Device
+
+var devices Results
+
+var ctx context.Context
+var err error
+
+// Initialization code
+func init() {
+	// devices = make([]Results, 0)
+	// Read JSON file and convert into an array of devices
+	file, err := ioutil.ReadFile("devices.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	_ = json.Unmarshal([]byte(file), &devices)
+
+}
+
+func ListDevicesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, devices)
+}
 
 func main() {
+	router := gin.Default()
 
+	router.GET("/devices", ListDevicesHandler)
+
+	router.Run()
 }
